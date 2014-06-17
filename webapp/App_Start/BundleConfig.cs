e1d71c487be9e80cf3197cb20a0a8960a81c5af7
Michael Pritchard <blueghostuk@gmail.com>
2014-06-17 23:03:28 +0100

    initial commit

---
+++ webapp/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace webapp.App_Start
+{
+    public static class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ratchet").Include(
+                "~/Scripts/ratchet.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                "~/Scripts/moment.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/*.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/ratchet.css",
+                "~/Content/ratchet-theme-ios.css",
+                "~/Content/app/site.css"));
+        }
+    }
+}
