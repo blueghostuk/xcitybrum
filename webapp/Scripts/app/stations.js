515e9751548880a22ff905240932a655428e6fd9
Michael Pritchard <blueghostuk@gmail.com>
2014-06-21 00:42:05 +0100

    update stations

--- webapp/Scripts/app/stations.js
+++ webapp/Scripts/app/stations.js
@@ -1,46 +1,46 @@
 ﻿var allStations = new Array();
 
-allStations.push(new TrainNotifier.XCityBrum.Station("LIF", "L'field T. Valley", { lat: 52, lng: 1 }, "Lichfield Trent Valley"));
+allStations.push(new TrainNotifier.XCityBrum.Station("LIF", "L'field T. Valley", "LIC", { lat: 52, lng: 1 }, "Lichfield Trent Valley"));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("LIC", "L'field City", { lat: 52, lng: 1 }, "Lichfield City"));
+allStations.push(new TrainNotifier.XCityBrum.Station("LIC", "L'field City", "LIF", { lat: 52, lng: 1 }, "Lichfield City"));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("SEN", "Shenstone", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("SEN", "Shenstone", "LIC", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("BKT", "Blake Street", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("BKT", "Blake Street", "SEN", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("BUL", "Butlers Lane", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("BUL", "Butlers Lane", "BKT", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("FOK", "Four Oaks", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("FOK", "Four Oaks", "BUL", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("SUT", "Sutton Coldfield", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("SUT", "Sutton Coldfield", "FOK", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("WYL", "Wylde Green", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("WYL", "Wylde Green", "SUT", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("CRD", "Chester Road", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("CRD", "Chester Road", "WYL", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("ERD", "Erdington", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("ERD", "Erdington", "CRD", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("GVH", "Gravelly Hill", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("GVH", "Gravelly Hill", "ERD", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("AST", "Aston", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("AST", "Aston", "GVH", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("DUD", "Duddeston", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("DUD", "Duddeston", "AST", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("BHM", "B'ham New St.", { lat: 52, lng: 1 }, "Birmingham New Street"));
+allStations.push(new TrainNotifier.XCityBrum.Station("BHM", "B'ham New St.", "FWY,ERD", { lat: 52, lng: 1 }, "Birmingham New Street"));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("FWY", "Five Ways", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("FWY", "Five Ways", "BHM", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("UNI", "University", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("UNI", "University", "FWY", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("SLY", "Selly Oak", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("SLY", "Selly Oak", "UNI", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("BRV", "Bournville", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("BRV", "Bournville", "SLY", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("KNN", "Kings Norton", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("KNN", "Kings Norton", "BRV", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("NFD", "Northfield", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("NFD", "Northfield", "KNN", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("LOB", "Longbridge", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("LOB", "Longbridge", "NFD", { lat: 52, lng: 1 }));
 
-allStations.push(new TrainNotifier.XCityBrum.Station("BTG", "Barnt Green", { lat: 52, lng: 1 }));
+allStations.push(new TrainNotifier.XCityBrum.Station("BTG", "Barnt Green", "LOB", { lat: 52, lng: 1 }));
 //# sourceMappingURL=stations.js.map
--- webapp/Scripts/app/stations.ts
+++ webapp/Scripts/app/stations.ts
@@ -3,112 +3,134 @@
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "LIF",
     "L'field T. Valley",
+    "LIC",
     { lat: 52, lng: 1 },
     "Lichfield Trent Valley"));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "LIC",
     "L'field City",
+    "LIF",
     { lat: 52, lng: 1 },
     "Lichfield City"));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "SEN",
     "Shenstone",
+    "LIC",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "BKT",
     "Blake Street",
+    "SEN",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "BUL",
     "Butlers Lane",
+    "BKT",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "FOK",
     "Four Oaks",
+    "BUL",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "SUT",
     "Sutton Coldfield",
+    "FOK",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "WYL",
     "Wylde Green",
+    "SUT",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "CRD",
     "Chester Road",
+    "WYL",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "ERD",
     "Erdington",
+    "CRD",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "GVH",
     "Gravelly Hill",
+    "ERD",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "AST",
     "Aston",
+    "GVH",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "DUD",
     "Duddeston",
+    "AST",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "BHM",
     "B'ham New St.",
+    "FWY,ERD", // to/from FWY and ERD
     { lat: 52, lng: 1 },
     "Birmingham New Street"));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "FWY",
     "Five Ways",
+    "BHM",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "UNI",
     "University",
+    "FWY",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "SLY",
     "Selly Oak",
+    "UNI",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "BRV",
     "Bournville",
+    "SLY",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "KNN",
     "Kings Norton",
+    "BRV",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "NFD",
     "Northfield",
+    "KNN",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "LOB",
     "Longbridge",
+    "NFD",
     { lat: 52, lng: 1 }));
 
 allStations.push(new TrainNotifier.XCityBrum.Station(
     "BTG",
     "Barnt Green",
+    "LOB",
     { lat: 52, lng: 1 }));
