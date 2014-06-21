2738ef416260160fce2544b1877a0c9f06c54d1a
Michael Pritchard <blueghostuk@gmail.com>
2014-06-22 00:04:03 +0100

    link back to correct station + update html

--- webapp/Scripts/app/pages/train.js
+++ webapp/Scripts/app/pages/train.js
@@ -4,8 +4,10 @@
     webApi.getService(serviceId).done(function (result) {
         var service = new TrainNotifier.XCityBrum.TrainDetailsResult(result);
 
+        ko.cleanNode($("#app-train-link")[0]);
         ko.cleanNode($("#app-train-title")[0]);
         ko.cleanNode($("#app-train-card")[0]);
+        ko.applyBindings(service, $("#app-train-link")[0]);
         ko.applyBindings(service, $("#app-train-title")[0]);
         ko.applyBindings(service, $("#app-train-card")[0]);
     });
--- webapp/Scripts/app/pages/train.ts
+++ webapp/Scripts/app/pages/train.ts
@@ -6,8 +6,10 @@ function loadTrain() {
         .done(function (result: GetServiceDetailsResult) {
             var service = new TrainNotifier.XCityBrum.TrainDetailsResult(result);            
 
+            ko.cleanNode($("#app-train-link")[0]);
             ko.cleanNode($("#app-train-title")[0]);
             ko.cleanNode($("#app-train-card")[0]);
+            ko.applyBindings(service, $("#app-train-link")[0]);
             ko.applyBindings(service, $("#app-train-title")[0]);
             ko.applyBindings(service, $("#app-train-card")[0]);
         });
