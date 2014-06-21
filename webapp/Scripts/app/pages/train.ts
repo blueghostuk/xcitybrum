8dfa7ddd2156a476d8bcba4bb2c9de140c508f21
Michael Pritchard <blueghostuk@gmail.com>
2014-06-21 11:36:41 +0100

    add initial train page js

---
+++ webapp/Scripts/app/pages/train.ts
@@ -0,0 +1,14 @@
+﻿
+function loadTrain() {
+    var serviceId = document.location.hash.substr(2);
+
+    webApi.getService(serviceId)
+        .done(function (result: GetServiceDetailsResult) {
+            var service = new TrainNotifier.XCityBrum.TrainDetailsResult(result);            
+
+            ko.cleanNode($("#app-train-title")[0]);
+            ko.cleanNode($("#app-train-card")[0]);
+            ko.applyBindings(service, $("#app-train-title")[0]);
+            ko.applyBindings(service, $("#app-train-card")[0]);
+        });
+} 
